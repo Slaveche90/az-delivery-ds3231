b719fd107c10774a585189217d34fcadff090976
Slavoljub Cvijanović <slavoljubcvijanovic@gmail.com>
2020-03-15 16:45:17 +0100

    Example script

--- rtc_example.py
+++ rtc_example.py
@@ -3,14 +3,14 @@ import rtc_lib
 
 degree_sign = u'\xb0'
 
-ds3231 = rtc.SDL_DS3231(1, 0x68)
+ds3231 = rtc_lib.SDL_DS3231(1, 0x68)
 ds3231.write_now() # saves the current date and time of R. Pi
 # ds3231.write_all(seconds=None, minutes=None, hours=None, day=None, date=None, month=None, year=None, save_as_24h=True) 
 # Range: seconds [0 - 59]; minutes [0 - 59]; hours [0 - 23]; day [1 - 7]; date [1 - 31]; month [1 - 12]; year [0 - 99]"""
 
 def check(num):
         '''A fucntion that put leading zero to single digit number
-           retrun: string
+           return: string
         '''
         if num < 10:
                 return "0{}".format(num)
